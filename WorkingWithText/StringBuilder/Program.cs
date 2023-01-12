136a715e5e021a7fde6b083fd09de40c72d15123
Adam Parker <dbassaultxx@gmail.com>
2023-01-12 15:15:32 -0500

    StringBuilder Commit

---
+++ WorkingWithText/StringBuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+/* StringBuilder
+ * 
+ * Defined in System.Text
+ * A Mutable string
+ * Easy and fast to crfeate and manipulate strings
+ * 
+ * Not for Searching
+ * 
+ * IndexOf(), LastIndexOf(), Contains(), StartsWith(),...
+ * 
+ * String Manipulation Methods
+ * 
+ * Append(), Insert(), Remove(), Replace(), Clear()
+ */
+
+var builder = new StringBuilder("hello world");
+
+builder
+    .Append('-', 10)
+    .AppendLine()
+    .Append("Header")
+    .AppendLine()
+    .Append('-', 10)
+    .Replace('-', '+')
+    .Remove(0, 10)
+    .Insert(0, new string('-', 10));
+
+Console.WriteLine(builder);
+
+Console.WriteLine($"First char: {builder[0]}");
